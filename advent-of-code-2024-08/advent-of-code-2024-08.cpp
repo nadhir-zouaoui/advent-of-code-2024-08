5212544f854eb219d954f5869663a69989ac5e69
nadhir zouaoui <nadhirzouaoui256@gmail.com>
2024-12-15 01:12:48 +0100

    problem2

--- advent-of-code-2024-08/advent-of-code-2024-08.cpp
+++ advent-of-code-2024-08/advent-of-code-2024-08.cpp
@@ -12,8 +12,8 @@ struct cordonnéesDuPoint
     int column;
 };
 int distinct(std::vector<char> t, char c) {
-    for (int i = 0;i<t.size();i++) {
-        if (t[i]==c)
+    for (int i = 0;i < t.size();i++) {
+        if (t[i] == c)
         {
             return false;
         }
@@ -21,8 +21,9 @@ int distinct(std::vector<char> t, char c) {
     return true;
 }
 int main() {
-    int maxRows=0;
+    int maxRows = 0;
     int maxColumns = 0;
+    int s = 0;
     std::ifstream file("input.txt");
     std::vector<std::vector<char>> fileMatrix;
     std::string line;
@@ -30,17 +31,18 @@ int main() {
     //declaring a table to hold the distinct letters of the test file so I can loop through myMap
     std::vector<char> TabOfdistinctCharecters;
     int i = 0;
-    int j=0;
+    int j = 0;
     int count = 0;
     while (std::getline(file, line)) {
         fileMatrix.push_back(std::vector<char>());
         for (j = 0; j < line.size(); j++) {
 
             fileMatrix[i].push_back(line[j]);
-            if (line[j]!='.')
+            if (line[j] != '.')
             {
+                s++;
                 myMap[line[j]].push_back({ i, j });
-                if (distinct(TabOfdistinctCharecters,line[j]))
+                if (distinct(TabOfdistinctCharecters, line[j]))
                 {
                     TabOfdistinctCharecters.push_back(line[j]);
                     count++;
@@ -51,14 +53,13 @@ int main() {
     }
     maxRows = i;
     maxColumns = j;
-    int s = 0;
     int testRow;
     int testColumn;
     for (i = 0; i < count; i++)
     {
         for (j = 0; j < myMap[TabOfdistinctCharecters[i]].size(); j++) {
             for (int k = 0; k < myMap[TabOfdistinctCharecters[i]].size(); k++) {
-                if (j!=k)
+                if (j != k)
                 {
                     int rowOfChar1 = myMap[TabOfdistinctCharecters[i]][j].row;
                     int columnOfChar1 = myMap[TabOfdistinctCharecters[i]][j].column;
@@ -66,18 +67,22 @@ int main() {
                     int columnOfChar2 = myMap[TabOfdistinctCharecters[i]][k].column;
                     int rowDiff = rowOfChar2 - rowOfChar1;
                     int columnDiff = columnOfChar2 - columnOfChar1;
-                    if (rowOfChar2 + rowDiff < maxRows && rowOfChar2 + rowDiff >= 0 &&
-                        columnOfChar2 + columnDiff < maxColumns && columnOfChar2 + columnDiff >= 0 &&
-                        fileMatrix[rowOfChar2 + rowDiff][columnOfChar2 + columnDiff] != '#')
+                    while (rowOfChar2 + rowDiff < maxRows && rowOfChar2 + rowDiff >= 0 &&
+                        columnOfChar2 + columnDiff < maxColumns && columnOfChar2 + columnDiff >= 0
+                        )
                     {
-                        s++;
+                        if (fileMatrix[rowOfChar2 + rowDiff][columnOfChar2 + columnDiff] == '.')
+                        {
+                            s++;
+                        }
                         fileMatrix[rowOfChar2 + rowDiff][columnOfChar2 + columnDiff] = '#';
+                        rowOfChar2 += rowDiff;
+                        columnOfChar2 += columnDiff;
                     }
-                } 
+                }
             }
         }
     }
-    
     std::cout << s;
 
     return 0;
